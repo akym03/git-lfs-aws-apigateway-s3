4303949f581dfdeddedd6d738606e00798a72b90
masahiro akiyama <akiyama@bk.iij4u.or.jp>
2020-05-05 13:04:16 +0900

    test_s3_datastore.py をリファクタ
    - MISSING のtypoを修正
    - makeUrl関数をテストクラスのスタティックメソッドに変更

--- tests/unit/test_s3_datastore.py
+++ tests/unit/test_s3_datastore.py
@@ -5,24 +5,23 @@ from botocore.exceptions import ClientError
 from git_lfs_aws_lambda.s3_datastore import S3Datastore
 
 TEST_BUCKET_NAME = "TEST_BUCKET_NAME"
-MESSING_KEY = "missingKey"
+MISSING_KEY = "missingKey"
 EXISTING_KEY = "existingKey"
-
-
-def makeUrl(operation, bucket, key):
-    return f"{operation}:{bucket}/{key}"
 
 
 class TestS3Datastore:
 
+    @staticmethod
+    def make_url(operation, bucket, key):
+        return f"{operation}:{bucket}/{key}"
+
     class S3Mock:
         def generate_presigned_url(self, operation, params, callback=None):
-            print(params)
-            if (operation == "put_object" and params['Key'] == MESSING_KEY):
-                return makeUrl(operation, params['Bucket'], params['Key'])
+            if (operation == "put_object" and params['Key'] == MISSING_KEY):
+                return TestS3Datastore.make_url(operation, params['Bucket'], params['Key'])
 
             if (operation == "get_object" and params['Key'] == EXISTING_KEY):
-                return makeUrl(operation, params['Bucket'], params['Key'])
+                return TestS3Datastore.make_url(operation, params['Bucket'], params['Key'])
 
             return "FakeError"
 
@@ -32,7 +31,7 @@ class TestS3Datastore:
                     "ContentLength": 64
                 }
 
-            if (params['Key'] == MESSING_KEY):
+            if (params['Key'] == MISSING_KEY):
                 error_response = {
                     "Error": {
                         "Code": "NotFound"
@@ -51,16 +50,16 @@ class TestS3Datastore:
         return None
 
     def test_should_produce_a_signed_upload_url(self):
-        given = MESSING_KEY
+        given = MISSING_KEY
         actual = self.datastore.get_upload_url(given)
 
-        assert actual == makeUrl("put_object", TEST_BUCKET_NAME, given)
+        assert actual == TestS3Datastore.make_url("put_object", TEST_BUCKET_NAME, given)
 
     def test_should_produce_a_signed_download_url(self):
         given = EXISTING_KEY
         actual = self.datastore.get_download_url(given)
 
-        assert actual == makeUrl("get_object", TEST_BUCKET_NAME, given)
+        assert actual == TestS3Datastore.make_url("get_object", TEST_BUCKET_NAME, given)
 
     def test_should_return_if_exists(self):
         given = EXISTING_KEY
@@ -69,7 +68,7 @@ class TestS3Datastore:
         assert actual is True
 
     def test_should_return_if_dose_not_exist(self):
-        given = MESSING_KEY
+        given = MISSING_KEY
         actual = self.datastore.exists(given)
 
         assert actual is False
