ff2770e5bd83ef5c41f078594e089723940dc667
masahiro akiyama <akiyama@bk.iij4u.or.jp>
2020-05-06 09:36:23 +0900

    lambda関数の list_locks を実装

---
+++ tests/integration/lambda_function/test_list_locks.py
@@ -0,0 +1,29 @@
+import json
+import re
+import pytest
+
+from git_lfs_aws_lambda.lambda_function.list_locks import lambda_handler
+from .create_request import request_with_body
+
+
+class TestListLocks:
+
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, mocker):
+        self.callback = mocker.Mock(return_value=None)
+
+    def test_is_not_implemented(self):
+        given = request_with_body({})
+
+        lambda_handler(given["event"], given["context"], self.callback)
+
+        self.callback.assert_called_once()
+
+        assert self.callback.call_args[0][0] is None
+        assert self.callback.call_args[0][1]["statusCode"] == 501
+
+        actual = json.loads(self.callback.call_args[0][1]["body"])
+        print(self.callback.call_args[0][1])
+        assert re.search('not implemented', actual["message"]) is not None
+        assert actual["request_id"] == "testRequestId"
+        assert actual["documentation_url"] is not None
